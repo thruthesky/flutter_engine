453b3557d97d15a690fc766c2d724ebd3e37dfc1
JaeHo Song <thruthesky@gmail.com>
2020-05-21 20:25:07 +0800

    ref

--- widgets/engine.post_list.dart
+++ widgets/engine.post_list.dart
@@ -1,6 +1,5 @@
-import 'package:clientf/flutter_engine/engine.comment.model.dart';
+
 import 'package:clientf/flutter_engine/engine.globals.dart';
-import 'package:clientf/flutter_engine/engine.post.model.dart';
 import 'package:clientf/services/app.service.dart';
 
 import './engine.post_item.dart';
@@ -11,16 +10,21 @@ class EnginePostList extends StatefulWidget {
   EnginePostList(
     this.forum, {
     @required this.onEdit,
+    @required this.onReply,
+    @required this.onDelete,
+    @required this.onError,
     Key key,
   }) : super(key: key);
 
   final EngineForum forum;
   final Function onEdit;
+  final Function onReply;
+  final Function onDelete;
+  final Function onError;
 
   @override
   _EnginePostListState createState() => _EnginePostListState();
 }
-
 
 
 class _EnginePostListState extends State<EnginePostList> {
@@ -34,13 +38,9 @@ class _EnginePostListState extends State<EnginePostList> {
         return EnginePostItem(
           widget.forum.posts[i],
           onEdit: widget.onEdit,
-          onReply: (EnginePost post) async {
-            var reply = await AppService.openCommentBox(post, null, EngineComment());
-            widget.forum.addComment(reply, post, null);
-            setState(() { /** 댓글 반영 */});
-          },
-          onDelete: () => AppService.alert(null, t('post deleted')),
-          onError: (e) => AppService.alert(null, t(e)),
+          onReply: widget.onReply,
+          onDelete: () => widget.onDelete,
+          onError: widget.onError,
         );
       },
     );
